5f420ec9ca03e3c6d56f3a97efe6493beed20ae9
Your Name <you@example.com>
2022-08-03 22:41:45 +0500

    Array (pop, push, shift, unshift, slice, splice)

---
+++ SMIT/Array 6 methods/app.js
@@ -0,0 +1,30 @@
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// document.write(arr)
+
+
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// arr.pop()
+// document.write(arr)
+
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// arr.push("G")
+// document.write(arr)
+
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// arr.shift()
+// document.write(arr)
+
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// arr.unshift("AA")
+// document.write(arr)
+
+
+// var arr = ["A", "B", "C", "D", "E", "F"]
+// arr.splice(3, 2, "C1")
+// document.write(arr)
+
+// var arr = ["A", "B", "C", "D", "E", "F"];
+// var newArr = arr.slice(1,4)
+// document.write(newArr)
+
+
